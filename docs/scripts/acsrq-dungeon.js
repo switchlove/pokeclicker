406b72300cc402d5ed522b5984dd19b7ef62fbe6
Switch <switchbladelove@gmail.com>
2022-08-29 08:10:45 -0500

    Rando - Script v0.0.15

---
+++ docs/scripts/acsrq-dungeon.js
@@ -0,0 +1,118 @@
+window.addEventListener('load', () => {
+    setTimeout(() => dungeonBot.interval = setInterval(dungeonBot, 100), 100);
+
+    // Reset boss position
+    const initializeDungeon = DungeonRunner.initializeDungeon;
+    DungeonRunner.initializeDungeon = function (dungeon) {
+        initializeDungeon.call(this, dungeon);
+        dungeonBot.boss = undefined;
+    };
+});
+
+function dungeonBot() {
+    if (!DungeonRunner.map || !clickEngagedD || !dungeonBot.isRunning()) {
+        return;
+    }
+
+    //Start dungeon if needed - by clicking on the button
+    if ( App.game.gameState == GameConstants.GameState.town) {
+        $('#townView button.btn-success')?.click();
+        dungeonBot.boss = undefined;
+    }
+
+    //Skip if in combat, catching or if the dungeon is not started yet
+    if (DungeonRunner.dungeonFinished() || DungeonRunner.fighting() || DungeonBattle.catching()) {
+        return;
+    }
+
+    const max = DungeonRunner.map.size - 1;
+    const playerPos = DungeonRunner.map.playerPosition();
+
+    //Tiles interaction
+    switch (DungeonRunner.currentTileType()()) {
+        case GameConstants.DungeonTile.chest:
+            if (Settings.getSetting('chestCollect').value && DungeonRunner.chestsOpened() < Settings.getSetting('maxChests').value) {
+                return DungeonRunner.handleClick();
+            }
+            break;
+        case GameConstants.DungeonTile.boss:
+            dungeonBot.boss = playerPos;
+            if (Settings.getSetting('botRush').value) {
+                return DungeonRunner.handleClick();
+            }
+            break;
+    }
+
+    //handle flash for boss rush
+    if (DungeonRunner.map.flash && Settings.getSetting('botRush').value) {
+        if (playerPos.y > 0 && DungeonRunner.map.board()[playerPos.y - 1][playerPos.x].type() == GameConstants.DungeonTile.boss) {
+            return DungeonRunner.map.moveUp();
+        }
+        if (playerPos.x > 0 && DungeonRunner.map.board()[playerPos.y][playerPos.x - 1].type() == GameConstants.DungeonTile.boss) {
+            return DungeonRunner.map.moveLeft();
+        }
+        if (playerPos.x < max && DungeonRunner.map.board()[playerPos.y][playerPos.x + 1].type() == GameConstants.DungeonTile.boss) {
+            return DungeonRunner.map.moveRight();
+        }
+    }
+
+    //Go to boss tile once everything is finished (or boss rush was enabled after the boss tile was found)
+    if (dungeonBot.boss && (Settings.getSetting('botRush').value || DungeonRunner.map.board().every(row => row.every(tile => tile.isVisited)))) {
+        if (DungeonRunner.map.hasAccessToTile(dungeonBot.boss)) {
+            DungeonRunner.map.moveToTile(dungeonBot.boss);
+            return DungeonRunner.handleClick();
+        }
+
+        for (let y = dungeonBot.boss.y; y <= max; y++) {
+            let pos = {x:dungeonBot.boss.x, y:y};
+            if (DungeonRunner.map.hasAccessToTile(pos)) {
+                return DungeonRunner.map.moveToTile(pos);
+            }
+        }
+    }
+
+    //Movement algorythme
+    if (!DungeonRunner.map.board().every(row => row.every(tile => tile.isVisited))) {
+        if (playerPos.y == max && !DungeonRunner.map.board()[max][0].isVisited) {
+            return DungeonRunner.map.moveLeft();
+        }
+        if (playerPos.x == max) {
+            return DungeonRunner.map.moveToCoordinates(0, playerPos.y - 1);
+        }
+        return DungeonRunner.map.moveRight();
+    }
+}
+
+//Check for dungeon options
+dungeonBot.isRunning = ko.pureComputed(() => {
+    if (!(player.town() instanceof DungeonTown)) {
+        return false;
+    }
+    if (App.game.gameState == GameConstants.GameState.dungeon) {
+        return true;
+    }
+
+    switch (Settings.getSetting('dungeOpts').observableValue()) {
+        case 'dungOptSC':
+            return !DungeonRunner.dungeonCompleted(player.town().dungeon, true);
+        case 'dungOptC':
+            return App.game.statistics.dungeonsCleared[GameConstants.getDungeonIndex(player.town().dungeon.name)]() < Settings.getSetting('maxClears').observableValue();
+        case 'dungOptDT':
+            return App.game.wallet.currencies[GameConstants.Currency.dungeonToken]() >= Settings.getSetting('minDT').observableValue();
+        default:
+            return true;
+    }
+});
+
+const showAllTiles = DungeonMap.prototype.showAllTiles;
+DungeonMap.prototype.showAllTiles = function () {
+    showAllTiles.call(this);
+
+    for (let y = 0; y < DungeonRunner.map.size; y++) {
+        for (let x = 0; x < DungeonRunner.map.size; x++) {
+            if (this.board()[y][x].type() == GameConstants.DungeonTile.boss) {
+                dungeonBot.boss = {x, y};
+            }
+        }
+    }
+};
--- docs/scripts/acsrq-ui.js
+++ docs/scripts/acsrq-ui.js
@@ -9,15 +9,7 @@ window.addEventListener('load', () => {
     // execute once save is fully loaded
     const gameStart = Game.prototype.start;
     Game.prototype.start = function() {
-        //#region Subscriptions
-        //reset bot state when bot are disabled
-        Settings.getSetting('botOptions').observableValue.subscribe((value) => {
-            if (!value) {
-                Settings.list.filter(s => s.name.startsWith('botstate.')).forEach(s => s.set(s.defaultValue));
-            }
-        });
-
-        //Disable/Enable event
+        //Disable/Enable event - need game to be fully loaded before triggering
         Settings.getSetting('disEvent').observableValue.subscribe((disable) => {
             for (let event of SpecialEvents.events) {
                 if (disable && event.hasStarted()) {
@@ -39,7 +31,6 @@ window.addEventListener('load', () => {
                 this.status = 2;
             }
         };
-        //#endregion
 
         gameStart.call(this);
     };
@@ -70,12 +61,12 @@ Settings.add(new Setting('gymOpts', 'Gym bot stop options', [
     new SettingOption('None', 'gymOptN'),
 ], 'gymOptN'));
 Settings.add(new Setting('gymE4Opts', 'E4 Boss to fight', [
-    new SettingOption('First', 1),
-    new SettingOption('Second', 2),
-    new SettingOption('Third', 3),
-    new SettingOption('Fourth', 4),
-    new SettingOption('Fifth', 5),
-], 1));
+    new SettingOption('First', '1'),
+    new SettingOption('Second', '2'),
+    new SettingOption('Third', '3'),
+    new SettingOption('Fourth', '4'),
+    new SettingOption('Fifth', '5'),
+], '1'));
 Settings.add(new Setting('bfOpts', 'Battle Frontier stop options', [
     new SettingOption('None', 'bfOptN'),
     new SettingOption('Time', 'bfOptT'),
@@ -94,9 +85,11 @@ Settings.add(new Setting('srOpts', 'Soft Reset Type', [
     new SettingOption('Shop Mon', 'poke'),
     new SettingOption('Regular Eggs', 'egg'),
 ], 'none'));
-Settings.add(new Setting('evoOpts', 'Soft Reset Evo Item',
-    Object.values(ItemList).filter(i => i instanceof EvolutionStone).map(e => new SettingOption(e.displayName, e.name)),
-    'Water_stone'));
+Settings.add(new RangeSetting('maxEggs', 'Max Eggs', 1, 4, 1, 1));
+Settings.add(new Setting('evoOpts', 'Soft Reset Evo Item', [
+    new SettingOption('All', ''),
+    ...Object.values(ItemList).filter(i => i instanceof EvolutionStone).map(e => new SettingOption(e.displayName, e.name)),
+], ''));
 Settings.add(new Setting('breedingOpts', 'Breeding options',[
     new SettingOption('None', 'none'),
     new SettingOption('Upto Attack', 'attack'),
@@ -113,9 +106,10 @@ Settings.add(new Setting('typedEggOpts', 'Typed egg to use', [
     new SettingOption('Fighting', 'fighting'),
     new SettingOption('Dragon', 'dragon'),
 ], 'fire'));
-Settings.add(new Setting('fossilOpts', 'Fossil to use',
-    UndergroundItems.list.filter(i => i.valueType == UndergroundItemValueType.Fossil).map(f => new SettingOption(f.displayName, f.name)),
-    'Dome Fossil'));
+Settings.add(new Setting('fossilOpts', 'Fossil to use',[
+    new SettingOption('All', ''),
+    ...UndergroundItems.list.filter(i => i.valueType == UndergroundItemValueType.Fossil).map(f => new SettingOption(f.displayName, f.name)),
+], ''));
 Settings.add(new Setting('evoItemCount', 'Evo items to use', [], 1));
 Settings.add(new Setting('ballBuyOpts', 'Auto-purchase pokeballs?', [
     new SettingOption('None', GameConstants.Pokeball.None),
@@ -139,6 +133,28 @@ Settings.add(new BooleanSetting('botstate.bf', 'BF Bot', false));
 Settings.add(new BooleanSetting('botstate.sr', 'SR Bot', false));
 Settings.add(new Setting('botstate.plant', 'Plant Bot', [new SettingOption('N/A', 'N/A')], 'N/A'));
 Settings.add(new Setting('botstate.mutate', 'Mutate Bot', [new SettingOption('N/A', 'N/A')], 'N/A'));
+//#endregion
+
+//#region Settings Subscriptions
+//reset bot state when bot are disabled
+Settings.getSetting('botOptions').observableValue.subscribe((value) => {
+    if (!value) {
+        Settings.list.filter(s => s.name.startsWith('botstate.')).forEach(s => s.set(s.defaultValue));
+    }
+});
+
+//srBot
+Settings.getSetting('botstate.sr').observableValue.subscribe((value) => {
+    clickEngagedSR = +value;
+
+    if (!value) {
+        localSettings({key:'', state: 0});
+    } else {
+        Settings.setSettingByName('disableSave', true);
+    }
+
+    localStorage.setItem(`settings${Save.key}`, JSON.stringify(Settings.toJSON()));
+});
 //#endregion
 
 //#region Info / Bot menu
@@ -234,7 +250,7 @@ acsrqInfo = function () {
         acsrqInfo.Checkbox('botstate.dungeon', 'App.game.keyItems.hasKeyItem(KeyItemType.Dungeon_ticket)', '!player.route() && player.town()?.dungeon'),
         acsrqInfo.Checkbox('botstate.gym', true, '!player.route() && player.town()?.content?.find(c => c instanceof Gym)'),
         acsrqInfo.Checkbox('botstate.safari', 'App.game.keyItems.hasKeyItem(KeyItemType.Safari_ticket)', 'Safari.inProgress()'),
-        acsrqInfo.Checkbox('botstate.sr','TownList[\'Route 3 Pokémon Center\'].isUnlocked()'),
+        acsrqInfo.Checkbox('botstate.sr','TownList[\'Route 3 Pokémon Center\'].isUnlocked() || App.game.breeding.canAccess()'),
         `<tr>${acsrqInfo.Row(
             'Pokeball',
             `<knockout data-bind="
@@ -363,6 +379,11 @@ acsrqSettings = function () {
                 acsrqSettings.Template('BooleanSettingTemplate', 'trackPhases'),
                 acsrqSettings.Number('phaseCount'),
             ]),
+        acsrqSettings.Section(
+            'Debug', [
+                acsrqSettings.Template2('Remote Script Version', `${rVer}`),
+                acsrqSettings.Template2('Local Script Version', `${lVer}`),
+            ], false),
     ];
     $('#settingsModal .nav-tabs')[0].insertAdjacentHTML('beforeend', '<li class="nav-item"><a class="nav-link" href="#settings-acsrq" data-toggle="tab">ACSRQ</a></li>');
     $('#settingsModal .tab-content')[0].insertAdjacentHTML('beforeend', `<div class="tab-pane" id="settings-acsrq">${acsrq.join('')}</div>`);
@@ -408,6 +429,7 @@ acsrqSettings = function () {
                 acsrqSettings.Template('MultipleChoiceSettingTemplate', 'fossilOpts', 'Settings.getSetting(\'srOpts\').observableValue() === \'fos\''),
                 acsrqSettings.Template('MultipleChoiceSettingTemplate', 'evoOpts', 'Settings.getSetting(\'srOpts\').observableValue() === \'evo\''),
                 acsrqSettings.Number('evoItemCount', 'Settings.getSetting(\'srOpts\').observableValue() === \'evo\''),
+                acsrqSettings.Range('maxEggs', '[\'fos\', \'egg\'].includes(Settings.getSetting(\'srOpts\').observableValue())'),
             ], false),
     ];
     $('#settingsModal .nav-tabs')[0].insertAdjacentHTML('beforeend', '<li class="nav-item" data-bind="visible: Settings.getSetting(\'botOptions\').observableValue"><a class="nav-link" href="#settings-acsrq-script" data-toggle="tab">ACSRQ Scripting</a></li>');
@@ -415,6 +437,7 @@ acsrqSettings = function () {
 };
 
 acsrqSettings.Template = (template, setting, visible = true) => `<tr data-bind="template: { name: '${template}', data: Settings.getSetting('${setting}')}, visible: ${visible}"></tr>`;
+acsrqSettings.Template2 = (template, setting, visible = true) => `<tr><td>${template}</td><td>${setting}</td></tr>`;
 
 acsrqSettings.Number = (setting, visible = true) => `
     <tr data-bind="template: { data: Settings.getSetting('${setting}') }, visible: ${visible}">
@@ -424,6 +447,12 @@ acsrqSettings.Number = (setting, visible = true) => `
                 data-bind="value: $data.observableValue(), attr: {name}"
                 onchange="Settings.setSettingByName(this.name, this.value)"/>
         </td>
+    </tr>
+`;
+
+acsrqSettings.Range = (setting, visible = true) => `
+    <tr data-bind="visible: ${visible}">
+        <td colspan="2" data-bind="template: { name: 'RangeSettingTemplate', data: Settings.getSetting('${setting}')}">
     </tr>
 `;
 
@@ -629,4 +658,19 @@ DungeonRunner.initializeDungeon = function(dungeon) {
         });
     }, 1);
 };
+//#endregion
+
+//#region BreedingSearchFilter
+Settings.add(new Setting('breedingSearchFilter', 'breedingSearchFilter', [], ''));
+
+Settings.getSetting('breedingSearchFilter').observableValue.subscribe((newValue) => {
+    const field = document.querySelector('#breeding-filter > div.form-group.col-md-6.col-6 > input');
+    if (`/(${field.value})/i` != newValue) {
+        field.value = newValue.slice(1,-1);
+        BreedingFilters.search.value(new RegExp(newValue, 'i'));
+    }
+});
+BreedingFilters.search.value.subscribe((newValue) => {
+    Settings.getSetting('breedingSearchFilter').set(newValue.source);
+});
 //#endregion
